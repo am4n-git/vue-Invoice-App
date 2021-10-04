614ead42c5363c027478f4011012178a766c769f
am4n-raj <am4nraj@gmail.com>
2021-10-04 18:27:21 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div >
+    <div id="app flex">
+      <Navigation />
+      <div class="app-content flex flex-column">
+          <router-view/>
+      </div>
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+import Navigation from "./components/Navigation.vue"
+export default {
+  component:{
+    Navigation
+  }
+}
+</script>
+
+
+
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #141625;
+  min-height: 100vh;
 }
 
 #nav {
